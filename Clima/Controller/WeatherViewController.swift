c80314adc3f072be4c30286b1f0ae3e17143337e
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2024-08-05 10:56:33 +0500

    working

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController , UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func searchPress(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
 }
 
